ab67ece54abbc6bf2f8b8db2ca9af2e46ab46c50
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-01-15 11:00:24 +0530

    Updated Solution - GfG to GitHub

--- Hard/Shortest Unique prefix for every word/shortest-unique-prefix-for-every-word.py
+++ Hard/Shortest Unique prefix for every word/shortest-unique-prefix-for-every-word.py
@@ -1,119 +1,63 @@
-#User function Template for python3
-class TrieNode:
-
-    def __init__(self):
-
-        self.children = {}
-
-        self.freq = 0
-
-class Solution:
-
-    def insert(self, root, st):
-
-        for i in range(len(st)):
-
-            c = st[i]
-
-            if c not in root.children:
-
-                root.children[c] = TrieNode()
-
-            root = root.children[c]
-
-            root.freq += 1
-
- 
-
-    def get_prefix(self, root, st):
-
-        pref = ""
-
-        for i in range(len(st)):
-
-            c = st[i]
-
-            if root.freq == 1:
-
-                break
-
-            pref += c
-
-            root = root.children[c]
-
-        return pref 
-
- 
-
-    def findPrefixes(self, arr, n):
-
-        root = TrieNode()
-
-        for word in arr:
-
-            self.insert(root, word)
-
-        ans = []
-
-        for word in arr:
-
-            ans.append(self.get_prefix(root, word))
-
-        return ans
 
 
-# class TrieNode():
+
+class TrieNode():
     
-#     def __init__(self):
-#         self.child={}
-#         self.isEnd=False
-#         self.childcount=0
+    def __init__(self):
+        self.child={}
+        self.isEnd=False
+        self.freq=0
         
         
-# class Solution:
-#     def findPrefixes(self, arr, N):
-#         # code here 
-#         root=TrieNode()
+class Solution:
+    def findPrefixes(self, arr, N):
+        # code here 
+        root=TrieNode()
         
-#         def ins(root, key):
+        def ins(root, key):
             
-#             curr=root
-#             for ch in key:
-#                 if ch not in curr.child:
-#                     curr.child[ch]=TrieNode()
-#                     curr.childcount+=1
+            curr=root
+            for ch in key:
+                if ch not in curr.child:
+                    curr.child[ch]=TrieNode()
                     
-#                 curr=curr.child[ch]
+                curr.freq+=1
+                curr=curr.child[ch]
                 
-#             curr.isEnd=True
+            curr.isEnd=True
             
         
-#         for i in arr:
-#             ins(root, i)
-            
-            
-#         def dfs(root, op):
-            
-#             if root.isEnd or root.childcount==1:
-#                 # op+=""
-#                 ans.append(op)
-#                 return 
+        for i in arr:
+            ins(root, i)
             
         
-#             for key in root.child:
-#                 op+=key
-#                 dfs(root.child[key], op)
-#                 op=op[:-1]
+            
+        def dfs(root, word):
+            
+            op="" 
+            
+            for ch in word:
                 
+                if root.isEnd or root.freq==1:
+                    break
+                op+=ch
+                root=root.child[ch]
+                
+            ans.append(op)
+                
+        
+        ans=[]
+        for i in arr:
+            dfs(root, i)
+            
+            
+        return ans
             
             
             
             
             
-            
-#         ans=[]
-#         dfs(root, "")
-#         return ans
+
 
 #{ 
  # Driver Code Starts
