3e239a187b45d7e2afe5835d1fc5184e4d29b394
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-01-24 01:39:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.py
+++ Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.py
@@ -7,38 +7,49 @@ class Solution:
         # code here
       
         def solve(i, j):
-            # if i==j:
-            #     return arr[i]
+            
             if i>=j:
                 return 0
                 
             if dp[i][j]!=-1:
                 return dp[i][j]
                 
-                
+            '''
+            when things happen to you, assume the worst
+            when you do things, you do the best
+            '''
+            
+            
+            
+            # 30 10 2 
+            
+            # maine 30 uthaya
+            
+            # voh ya toh 10 utahayega toh muje 2 milega
+            # ya 2 uthayega toh muje 10 milega
+            
+            # muje voh min dega so voh 10 le liya and muje 2 milla
+            
             left=arr[i]+min(solve(i+2, j),solve(i+1, j-1))
                 
-            # if arr[i+1]>arr[j]:
-            #     left=arr[i] + solve(i+2, j)
-            # else:
-            #     left=arr[i] + solve(i+1, j-1)
                 
+            # Similarly yaha se ill check
+            # maine 2 uthaya ...
+            
             right=arr[j]+min(solve(i+1, j-1),solve(i, j-2))
                 
-            # if arr[i]>arr[j-1]:
-            #     right=arr[j] + solve(i+1, j-1)
-            # else:
-            #     right=arr[j] + solve(i, j-2)
-                
             
             
-            
+            # aur un dono ka max is ans
             dp[i][j]= max(left, right)
             return dp[i][j]
             
             
         dp=[[-1]*n for i in range(n)] 
         return solve(0, n-1)
+
+
+
 
 
 #{ 
