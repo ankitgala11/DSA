6a7d4583a7650f6892843a756d7cd771ced66813
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-06-01 18:30:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kadane's Algorithm/kadanes-algorithm.py
+++ Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -6,17 +6,23 @@ class Solution:
     def maxSubArraySum(self,arr,N):
         ##Your code here
         
-        currsum=arr[0]
-        ans=currsum
+        ans = arr[0]
+        curr = arr[0]
+        
+     
         
         for i in range(1, N):
-            currsum+=arr[i]
             
-            currsum=max(currsum, arr[i])
-            ans=max(ans, currsum)
+            
+            curr = curr+arr[i]
+         
+            curr = max( curr , arr[i] )
+            ans = max(ans, curr)
+           
             
             
         return ans
+            
 
 
 #{ 
